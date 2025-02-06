adea7974fae7a29c50d1fc5cb32e6b3cfa71b839
IanL73 <ianl73@nycstudents.net>
2025-02-06 09:06:51 -0500

    balls

--- data.py
+++ data.py
@@ -1,4 +1,21 @@
-x = 3   # the difference between integers and floats in python
+""" x = 3   # the difference between integers and floats in python
 y = float(3)
-print(x,y)
+print(x,y) """
 
+""" values = [1,2.23,5,7,2,30,15] # this is a list. It contains numbers. Obviously. Important to note, it does not discriminate between Integers and Floats. Pretty Handy. """
+""" print(values) # using "print" on the name of the list prints the whole list (brackets included).
+for i in values: # this prints every value in it's own line.
+    print(i) """
+
+""" print(values[6]) # this function lets you print various values individually. Remember that CS starts counting at 0. """
+
+# Strings! Strings hold "text" values. Strings aren't "just text", each word is actually a list consisting of each letter in it.
+# for example, "test" is ["t","e","s","t"]
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+sent = input("Give me a sentence")
+print(sent)
