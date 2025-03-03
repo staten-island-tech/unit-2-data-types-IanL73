da2bf04b52b401b0a7d13f1b4f3d67da22fe1a87
IanL73 <ianl73@nycstudents.net>
2025-03-03 08:44:47 -0500

    big balls

--- data.py
+++ data.py
@@ -17,6 +17,7 @@ z = y[0]
 print(y)
 print(z) """
 
+#Challenge "Words in sentence"
 """ sent = input("Give me a sentence") # this function takes a sentence and tells you how many words are in it I'm very proud of it :3
 wordlist = sent.split( )
 wordnumber = 0
@@ -24,7 +25,8 @@ for i in wordlist:
     wordnumber = wordnumber + 1
 print(wordnumber) """
 
-# Now it's time for the madlibs project. yeehaw.
+#Challenge "Madlibs"
+""" # Now it's time for the madlibs project. yeehaw.
 name = input("Give me a name. It doesn't have to be yours, just anyone you think is interesting.")
 verb1 = input("Give me a verb.")
 adjective1 = input("Give me an adjective.")
@@ -36,12 +38,12 @@ verb3 = input("Give me a third verb.")
 noun2 = input("Give me another noun.")
 adjective3 = input("Give me a third adjective.")
 guest = input("Give me a famous person. They don't have to be real, they just need to be popular.")
-verb4 = input("Give me a fourth verb, I promist the is the last one.")
+verb4 = input("Give me a fourth verb, I promise the is the last one.")
 # put more things here if you need them, kay ;3
 
 # okay so the story's going to sound like this:
 #  Once upon a time, there lived one "(instert name)". (Instert Name) was taking nice stroll through the fields, enjoying their time to the fullest. After taking a spell to (verb1), they set their sights on a(n) (adjective1), yet inviting woods. upon walking in, they find themself transported to a world of wonder, filled with whimsical creatures, (verb2)ing (noun1)s, and magic glowing lights of no discernable origin. (Name) begins to wander, taking in all the sights, smells, and sounds of the forest, leaving them completely vulnerable, and oblivious, to the dangers that lurk just out of sight. Suddenly, (Name) is a attacked by a ferocious pack of (numeral), (adjective2), (verb3)ing (noun2)s!! Oh! The horror! Just as (name) is coming to terms with their untimly and, frankly, unceremonious demise, a miracle occurs! (guest) appears from the brush, striking down each and every (noun2) with grace and skill. (guest) helps (name) up and, after making sure they're okay, walks home with them, the two (verb4)ing all the way. The End.
-print("Once upon a time, there lived one",name+".",name,"was taking nice stroll through the fields, enjoying their time to the fullest. After taking a spell to",verb1+", they set their sights on a(n)",adjective1+", yet inviting woods. Upon walking in, they find themself transported to a world of wonder, filled with whimsical creatures,",verb2+"ing",noun1+"s, and magic glowing lights of no discernable origin.",name,"begins to wander, taking in all the sights, sounds, and smells of the forest, leaving them completely oblivious to the dangers lurking just out of sight. Suddenly,",name,"is attacked by a ferocious pack of",numeral+",",verb3+"ing",noun2+"s! Oh! The horror! Just as",name,"is coming to terms with their untimely and, frankly, quite unceremonious demise, a miracle occurs!",guest,"appears from the brush!, striking down each and every",noun2,"with the grace of a butterfly, and cruel precision of a... a uh... like, some kind of... precision, animal. Or something. Anyway,",guest,"helps",name,"up and, after making sure they're okay, walks home with them, the two,",verb4+"ing all the way. The End.")
+print("Once upon a time, there lived one",name+".",name,"was taking nice stroll through the fields, enjoying their time to the fullest. After taking a spell to",verb1+", they set their sights on a(n)",adjective1+", yet inviting woods. Upon walking in, they find themself transported to a world of wonder, filled with whimsical creatures,",verb2+"ing",noun1+"s, and magic glowing lights of no discernable origin.",name,"begins to wander, taking in all the sights, sounds, and smells of the forest, leaving them completely oblivious to the dangers lurking just out of sight. Suddenly,",name,"is attacked by a ferocious pack of",numeral+",",verb3+"ing",noun2+"s! Oh! The horror! Just as",name,"is coming to terms with their untimely and, frankly, quite unceremonious demise, a miracle occurs!",guest,"appears from the brush!, striking down each and every",noun2,"with the grace of a butterfly, and cruel precision of a... a uh... like, some kind of... precision, animal. Or something. Anyway,",guest,"helps",name,"up and, after making sure they're okay, walks home with them, the two,",verb4+"ing all the way. The End.") """
 
 """ #intro to bouillon. I love soup.
 day_of_week = input("what day is it? ")
@@ -62,6 +64,7 @@ elif temp == 68:
 else:
     print('cold') """
 
+#Challenge "Odd or Even"
 """ # odd or eveniffyer
 number = int(input("gimme a number."))
 remainder = number % 2
@@ -70,6 +73,7 @@ if remainder == 0:
 else:
     print("Arite, broski, I'm sorry, but that number, aint even. It's as odd as a dog eating icecream on a skateboard peeps.") """
 
+#Challenge "Tip Calculator"
 """ # Tip calculator
 Bill = float(input("What's your bill."))
 while True:
@@ -101,6 +105,7 @@ while True:
     else:
         print("that's not a valid rating. it needs to be either Good, Great, Okay, or Bad. No spaces, just input it directly into the terminal. Let's try that again.") """
 
+#Challenge "Factors"
 """ # Factor-Finder 3000
 num = int(input("NUMBER. NOW."))
 for i in range(num):
@@ -110,7 +115,8 @@ for i in range(num):
     else:
         num = num """
 
-""" # Factor-Finder 2.0
+#Challenge "Greatest Common Factor"
+# Factor-Finder 2.0
 step1 = 1
 factlist = [] # list of common factors
 num1 = int(input("NUMBER. NOW.")) # the first number
@@ -134,4 +140,4 @@ for i in range(num1): # check number 1's factor set
                 step2 = step2 + 1
     else:
         step1 = step1 + 1
-print(factlist[0]) """
+print(factlist[0])
