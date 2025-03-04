874e4256a22059df4c76097de70e1cccfc198c3a
IanL73 <ianl73@nycstudents.net>
2025-03-04 09:03:06 -0500

    MY BALLS HURT

--- bank.py
+++ bank.py
@@ -1,14 +1,22 @@
-money = 500
-chuck = True
-while chuck == True:
+""" money = 500
+while True:
     chuck = input("would you like to withdraw?")
     if chuck == "exit" or chuck == "no":
         print("Goodbye!")
-        chuck = False
+        break
     else:
         draw = int(input("How much withdraw, boy."))
         if draw > money:
             print("You don't have that much, boy.")
         else:
             money = money - draw
-            print("You have $",money,"left in savings.")
+            print("You have $",money,"left in savings.") """
+
+# Python 3: Fibonacci series up to n
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+fib(1000000)
